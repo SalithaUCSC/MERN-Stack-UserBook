75c0a5325fba688f5812025df0e25cd6e698cddc
Salitha Chathuranga <salithachathuranga94@gmail.com>
2019-02-03 09:36:46 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 // Initialize app
 const app = express();
 // Initialize port
-const port = 4000;
+const port = 3000 || process.env.PORT;
 // Initialize paths
 const path = require('path');
 
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Mongoose connection
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/api');
+mongoose.connect('mongodb://salitha:salitha94@ds111622.mlab.com:11622/node_api');
 const db = mongoose.connection;
 
 // Check for DB connection
@@ -44,6 +44,6 @@ app.get('/', function(req, res){
     res.send('Hello from Server');
 })
 
-app.listen(port, function(){
-    console.log('server started at port ' + port);
+app.listen(process.env.PORT || 5000, function(){
+    console.log('server started...');
 })
